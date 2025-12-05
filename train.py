a1cfa4394baa7a8f40dc51502ba4425002a1f7bd
Sunflower <lijiaxin030520@outlook.com>
2025-12-05 17:32:47 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -7,38 +7,35 @@ from pydoc import locate
 
 import numpy as np
 import torch
+# import torchzzz
 import torch.backends.cudnn as cudnn
 
 from trainer import trainer_synapse
 
 warnings.filterwarnings("ignore")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--root_path",
     type=str,
     default="./data/Synapse/train_npz",
-    # default="/ljx/data3/DAEFormer/data/Synapse/train_npz",   # gai 路径
     help="root dir for train data",
 )
 parser.add_argument(
     "--test_path",
     type=str,
     default="./data/Synapse/test_vol_h5",
-    # default="/ljx/data3/DAEFormer/data/Synapse/test_vol_h5",   # gai 路径
     help="root dir for test data",
 )
 parser.add_argument("--dataset", type=str, default="Synapse", help="experiment_name")
 parser.add_argument("--list_dir", type=str, default="./lists/lists_Synapse", help="list dir")
 parser.add_argument("--num_classes", type=int, default=9, help="output channel of network")
 parser.add_argument("--output_dir", type=str, default="./model_out", help="output dir")
-parser.add_argument("--max_iterations", type=int, default=90000, help="maximum epoch number to train")  # gai 2.3 90000→190000
-parser.add_argument("--max_epochs", type=int, default=200, help="maximum epoch number to train")
+parser.add_argument("--max_iterations", type=int, default=900000, help="maximum epoch number to train")
+parser.add_argument("--max_epochs", type=int, default=580, help="maximum epoch number to train")
 parser.add_argument("--batch_size", type=int, default=20, help="batch_size per gpu")
 parser.add_argument("--num_workers", type=int, default=4, help="num_workers")
-parser.add_argument("--eval_interval", type=int, default=20, help="eval_interval")    # 模型训练时每隔多少个epoch对验证集进行一次评估
+parser.add_argument("--eval_interval", type=int, default=20, help="eval_interval")
 parser.add_argument("--model_name", type=str, default="synapse", help="model_name")
 parser.add_argument("--n_gpu", type=int, default=2, help="total gpu")
 parser.add_argument("--deterministic", type=int, default=1, help="whether to use deterministic training")
@@ -71,25 +68,17 @@ parser.add_argument(
 parser.add_argument("--tag", help="tag of experiment")
 parser.add_argument("--eval", action="store_true", help="Perform evaluation only")
 parser.add_argument("--throughput", action="store_true", help="Test throughput only")
-# parser.add_argument(
-#     "--module", default="networks.DAEFormer_DIFF_decoder_SGFN.DAEFormer.DAEFormer",help="The module that you want to load as the network, e.g. networks.DAEFormer.DAEFormer"
-# )   # gai 12.23 module default
 parser.add_argument(
-    "--module", default="networks.DAEFormer_dual_decoder_SGFN.DAEFormer",help="The module that you want to load as the network, e.g. networks.DAEFormer.DAEFormer"
+    "--module", default="networks.DFAFormer.DFAFormer",help="The module that you want to load as the network, e.g. networks.DAEFormer.DAEFormer"
 )   # gai 12.23 module default
-
-# gai 9.26: 添加 --use_patch 参数
-# parser.add_argument("--use_patch", action="store_true", help="Use patch-based training with full-res input")
-parser.add_argument('--use_patch', action='store_true',
-                    help='use random patch cropping during training')
 
 args = parser.parse_args()
 
-# gai 10.8
+
 if __name__ == "__main__":
     # setting device on GPU if available, else CPU
     transformer = locate(args.module)
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
     print()
 
@@ -130,11 +119,9 @@ if __name__ == "__main__":
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    net = transformer(num_classes=args.num_classes).cuda()
-    # net = transformer(num_classes=args.num_classes, image_size=args.img_size).cuda()  # gai 9.26
+    net = transformer(num_classes=args.num_classes).cuda(0)
 
     trainer = {
         "Synapse": trainer_synapse,
     }
     trainer[dataset_name](args, net, args.output_dir)
-
